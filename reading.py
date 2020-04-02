95f3472bf6635bf86f73e6ee592218aecd903137
Priyanshu Yadav <56887107+pr-yadav@users.noreply.github.com>
2020-04-03 03:53:51 +0530

    Update reading.py

--- reading.py
+++ reading.py
@@ -3,19 +3,56 @@ import requests
 import string as str
 import csv
 
+
+#func to check for space between name
 def oneword(str):
     for x in range(len(str)):
         if str[x]==32 :
-            flag2=1
+            flag2=0
 
-flag0=0
+#function to check for special char
+def specialchar(str):
+    for x in range(len(str)):
+        if not((str[x]>='A' and str[x]<="Z")or(str[x]>='a' and str[x]<='z')) :
+            flag1=1
+
 flag1=0
-flag2=0
+flag2=1
+str={}
+
+
+#taking input and printing invalid names
+#-----------------------------------------------------------------
 
 jfile = input('Enter json file path : ')
+cfile = input('Enter csv file path : ')
+with open(cfile, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        str=row['Name']
+        oneword(row['Name'])
+        specialchar(row['Name'])
+        if ((row['Name']).islower() or not flag2  or flag1):
+            print(row['Name'])
+        
+        flag1=0
+        flag2=1
+#-----------------------------------------------------------------
+
+
+
+#printing the data
+
 j=open(jfile)
 jdata = json.load(j)
 for data in jdata : 
     print(data['n'],end=" ")
     print(data['i'],end=" ")
-    print(data['p'])
+    print(data['p'],end=" ")
+    for row in reader:
+        if data['n']==row['Name'] :
+            print(row['Organisation'] ,end=" ")
+            print(row['Project'])
+
+
+#--------------------------------------------------
